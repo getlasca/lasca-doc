e824b8b7b1c9cf451009c3eff371810fa787b161
kazuma ueda <ueda1023@gmail.com>
2021-11-05 18:16:23 +0900

    setup config file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Lasca Documentation',
+  description: 'No more front-end coding by integrating Figma with React/vue.js/webpack',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'Go to Lasca', link: 'http://lasca.app/', rel:false }
+    ],
+    sidebar: [
+      '/'
+    ],
+    searchPlaceholder: 'Search...',
+    smoothScroll: true
+  }
+}
